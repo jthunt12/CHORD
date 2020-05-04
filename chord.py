06f2b58f4f7537394920ab8d6eb4afbe480c5a45
Troy Hunt <36526335+jthunt12@users.noreply.github.com>
2020-05-03 23:24:01 -0400

    Update chord.py

--- chord.py
+++ chord.py
@@ -11,7 +11,7 @@
 #   In computing, Chord is a protocol and algorithm for a peer-to-peer distributed hash table.
 #   A distributed hash table stores key-value pairs by assigning keys to different computers
 #   (known as "nodes")
-#   a node will store the values for all the keys for which it is responsible.
+#   A node will store the values for all the keys for which it is responsible.
 #   Chord specifies how keys are assigned to nodes, and how a node can discover the value for a given
 #   key by first locating the node responsible for that key.
 #
